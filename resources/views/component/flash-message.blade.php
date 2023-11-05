439aa519e1ff3f8c62f4dc24a60fec1eb0e3cf38
Emmanuel Akinyemi Oluwadamilare <93402402+EmmanuelAkinyemi@users.noreply.github.com>
2023-11-05 14:00:50 +0100

    flash message updated

--- resources/views/component/flash-message.blade.php
+++ resources/views/component/flash-message.blade.php
@@ -1,12 +1,18 @@
-@if (session()->has('message'))
-<div
-    x-init="setTimeout(()=> show = false, 3000)"
-    x-data="{show: true}"
-    x-show="show"
-    class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-red-500 text-white px-48 py-3">
-    <p>{{ session('message') }}</p>
+<!-- component -->
+<div class='flex flex-row bg-gray-900 h-10 w-[400px] rounded-[30px]'>
+	<span class='flex flex-col justify-center text-white font-bold grow-[1] max-w-[90%] text-center'>Your request has been submitted</span>
+	<div class='w-[10%] bg-green-400 rounded-r-2xl shadow-[0_0_20px_#00C85177]'></div>
 </div>
-@endif
+<hr class='my-3'>
+<div class='flex flex-row bg-gray-900 h-10 w-[400px] rounded-[30px]'>
+	<span class='flex flex-col justify-center text-white font-bold grow-[1] max-w-[90%] text-center'>Your request has been denied</span>
+	<div class='w-[10%] bg-red-400 rounded-r-2xl shadow-[0_0_20px_#ff444477]'></div>
+</div>
+<hr class='my-3'>
+<div class='flex flex-row bg-gray-900 h-10 w-[400px] rounded-[30px]'>
+	<span class='flex flex-col justify-center text-white font-bold grow-[1] max-w-[90%] text-center'>Missing information</span>
+	<div class='w-[10%] bg-yellow-400 rounded-r-2xl shadow-[0_0_20px_#ffbb3377]'></div>
+</div>
 
 
 
