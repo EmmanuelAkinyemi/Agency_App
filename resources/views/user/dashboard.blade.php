1bd92e76dd2fc841ebbc1667e806dd6df5bf186b
Emmanuel Akinyemi Oluwadamilare <93402402+EmmanuelAkinyemi@users.noreply.github.com>
2023-11-24 20:09:30 +0100

    Update dashboard.blade.php

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -13,7 +13,6 @@
         <div class="row">
             <div class="col-xl-6 col-xxl-12">
                 <div class="row">
-
                     <div class="col-sm-12 col-md-6">
                         <div class="card">
                             <div class="card-body">
@@ -28,6 +27,29 @@
                                                 </p>
 
 
+                                        </div>
+
+                                        <div class="d-inline-block position-relative donut-chart-sale">
+                                            <span class="donut1"
+                                                data-peity='{ "fill": ["rgb(60, 76, 184)", "rgba(236, 236, 236, 1)"],   "innerRadius": 38, "radius": 10}'></span>
+                                            <small class="text-primary"></small>
+                                            <span class="circle bgl-primary"></span>
+                                        </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="media align-items-center">
+                                        <div class="media-body me-3">
+                                                <h2 class="fs-36 text-black font-w600">
+                                                    ${{ $activeDeposit }}
+                                                </h2>
+                                                <p class="fs-18 mb-0 text-black font-w500">Active Deposit
+                                                </p>
                                         </div>
 
                                         <div class="d-inline-block position-relative donut-chart-sale">
@@ -60,30 +82,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-12 col-md-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="media align-items-center">
 
-                                        <div class="media-body me-3">
-                                                <h2 class="fs-36 text-black font-w600">
-                                                    ${{ $activeDeposit }}
-                                                </h2>
-                                                <p class="fs-18 mb-0 text-black font-w500">Active Deposit
-                                                </p>
-                                        </div>
-
-                                        <div class="d-inline-block position-relative donut-chart-sale">
-                                            <span class="donut1"
-                                                data-peity='{ "fill": ["rgb(60, 76, 184)", "rgba(236, 236, 236, 1)"],   "innerRadius": 38, "radius": 10}'></span>
-                                            <small class="text-primary"></small>
-                                            <span class="circle bgl-primary"></span>
-                                        </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div class="col-sm-12 col-md-6">
                         <div class="card">
                             <div class="card-body">
